3e5f7d8c81fd988dd538f4231c3a40f56cf89769
Albert <hr00090241@gmail.com>
2016-03-31 18:06:40 +0800

    req

--- util.js
+++ util.js
@@ -57,5 +57,31 @@ u.ready = function ready(fn) {
 			}
 		})
 	}
+};
+
+u.ajax = function ajax(url,fn) {
+	var req;
+
+	if(window.XMLHttpRequest) { // Mozilla, Safari,chrome
+		req = new XMLHttpRequest();
+		if(req.overrideMimeType) {
+			req.overrideMimeType('text/xml'); //避免部分版本的 Mozilla 瀏覽器，在伺服器送回的資料未含 XML mime-type 檔頭（header）時會出錯。
+		}
+	} else if (window.ActiveXObject) { //IE
+		req = new ActiveXObject('Microsoft.XMLHttp');
+	}
+
+	if(!req) {
+		console.log('Cannot create an XMLHTTP instance');
+	}
+
+	req.onreadystatechange = function () {
+		fn(req);
+	};
+
+	req.open('GET',url,true); 
+		//第三個參數 如果設定為 TRUE 則即使伺服器尚未傳回資料也會繼續執行其餘的程式 
+		//存取資料的方式 有兩種 httpRequest.responseText把傳回值視為字串用 跟 httpRequest.responseXML把傳回值視為 XMLDocument 物件
+	req.send('');
 }
 
